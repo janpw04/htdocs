962df1789a74c267effcfe43521d8df5bf77bc2c
Jan Wagner <janpaulwagner2004@gmail.com>
2023-07-30 00:44:52 +0200

    just the basic webiste without any cool shit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Function to check if the device is a mobile phone
+function isMobile() {
+    return window.innerWidth <= 768; // Adjust the width as needed
+}
+
+// Function to add a class to the <body> tag based on device type
+function addDeviceClass() {
+    if (isMobile()) {
+        document.body.classList.add('mobile');
+    } else {
+        document.body.classList.add('desktop');
+    }
+}
+
+// Call the function on page load
+addDeviceClass();
