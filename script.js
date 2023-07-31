1b86b6e1948c0b096f52c455d36252e11b452d23
Jan Wagner <janpaulwagner2004@gmail.com>
2023-07-31 16:47:44 +0200

    login logik implimentiert

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const login_button = document.getElementById("login_button");
 const login_error_message = document.getElementById("error_message");
-const u = null;
+var user;
 
 function loginButtonClicked(){
     const log_username = document.getElementById("inputUsername").value;
     const log_password = document.getElementById("inputPassword").value;
 
     console.log(log_username + log_password);
-
-    if(log_username === "user" && log_password === "pw") {
+    
+    if(log_username === user.cUsername && log_password === user.cPassword || log_username === 'user' && log_password === 'pw') {
         alert("You have successfully logged in!");
         console.log("You have successfully logged in!")
         location.reload(); //Can be changed to whatever should happen if data correct
@@ -19,19 +19,35 @@ function loginButtonClicked(){
 }
 
 function registerButtonClicked(){
-    var reg_username = document.getElementById("inputUsername").value;
-    var reg_password = document.getElementById("inputPassword").value;
+    const reg_username = document.getElementById("inputUsername").value;
+    const reg_password = document.getElementById("inputPassword").value;
 
-    u = new user(reg_username);
+    const u = new User(reg_username, reg_password);
 
-    console.log(reg_username + reg_password);
+    console.log(u.getUsername);
+    console.log(u.getPassword);
 
-    if(reg_username === "user" && reg_password === "pw") {
-        alert("You have successfully logged in!");
-        console.log("You have successfully logged in!")
-        location.reload(); //Can be changed to whatever should happen if data correct
-    } else {
-        console.log("Wrong username or password");
-        login_error_message.style.opacity = 1;
+    user = u;
+
+    console.log(user)
+}
+
+class User{     
+    constructor(reg_username, reg_password) {
+        this.cUsername = reg_username;
+        this.cPassword = reg_password;
+        localStorage.setItem(reg_username, reg_password);
+    }
+
+    get getUsername() {
+        return this.cUsername;
+    }
+    get getPassword() {
+        return this.cPassword;
+    }
+
+    setUsernameAndPassword(reg_username, reg_password) {
+        this.cUsername = reg_username;
+        this.cPassword = reg_password;
     }
 }
